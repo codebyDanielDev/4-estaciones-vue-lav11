747d45401f4cd5569f350bfc84d766aef7e7ad20
Carlos Daniel Quispe Quispe <quispecarlosdaniel078@gmail.com>
2024-06-16 22:43:59 -0500

    pe causa

--- resources/js/Pages/App/Calculate/Index.vue
+++ resources/js/Pages/App/Calculate/Index.vue
@@ -43,79 +43,129 @@ const openAddProductModal = () => {
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
 
+
+
+
+
+
+
+
+
+
+
+
+
+                <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
                     <div class="py-12">
                         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                             <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
-                                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                                    <thead class="bg-gray-50 dark:bg-gray-700">
-                                        <tr>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                                                Producto
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                                                Divisor
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                                                % Minimo
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                                                % Maximo
-                                            </th>
-                                            <th scope="col"
-                                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                                                Cantidad
-                                            </th>
-
-                                            <th scope="col"
-                                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
-                                                Precio Total
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody
-                                        class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                                        <tr>
-                                            <td
-                                                class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-gray-200">
-                                                NOMBRE
-                                            </td>
-                                            <td
-                                                class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
-                                                DIVISOR
-                                            </td>
-                                            <td
-                                                class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
-                                                Minimo
-                                            </td>
-                                            <td
-                                                class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
-                                                Maximo
-                                            </td>
-
-                                            <td
-                                                class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
-                                                <input type="number"
-                                                    class="block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300" />
-                                            </td>
-
-                                            <td
-                                                class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
-                                                <input type="text"
-                                                    class="block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300" />
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                <div class="py-12">
+                                    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                                        <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
+                                            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                                <thead class="bg-gray-50 dark:bg-gray-700">
+                                                    <tr>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
+                                                            Producto
+                                                        </th>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
+                                                            Divisor
+                                                        </th>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
+                                                            % Minimo
+                                                        </th>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
+                                                            % Maximo
+                                                        </th>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
+                                                            Cantidad
+                                                        </th>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
+                                                            Precio Total
+                                                        </th>
+                                                        <th scope="col"
+                                                            class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-gray-300">
+                                                            Acción
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody
+                                                    class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                                    <tr>
+                                                        <td
+                                                            class="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-gray-200">
+                                                            NOMBRE
+                                                        </td>
+                                                        <td
+                                                            class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
+                                                            DIVISOR
+                                                        </td>
+                                                        <td
+                                                            class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
+                                                            Minimo
+                                                        </td>
+                                                        <td
+                                                            class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
+                                                            Maximo
+                                                        </td>
+                                                        <td
+                                                            class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
+                                                            <input type="number"
+                                                                class="block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300" />
+                                                        </td>
+                                                        <td
+                                                            class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
+                                                            <input type="text"
+                                                                class="block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300" />
+                                                        </td>
+                                                        <td
+                                                            class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-300">
+                                                            <button
+                                                                class="px-4 py-2 font-semibold text-white bg-red-500 rounded-md shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                                                Eliminar
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colspan="7"
+                                                            class="px-6 py-4 text-sm text-center text-gray-500 whitespace-nowrap dark:text-gray-300">
+                                                            <button
+                                                                class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                                                Agregar producto
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                            <button
+                                                class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                                Calcular
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
+
+
+
+
+
+
+
+
+
+
             </div>
         </div>
     </AppLayout>
